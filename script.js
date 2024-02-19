60c4d18d3c2d6382c753b262324fbac7eb0c8231
Danny <dannylau@Dannys-MacBook-Air.local>
2024-02-19 09:53:16 -0800

    add checking winner and tie logic

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const Gameboard = (function () {
+const GameBoard = (function () {
     const board = [
         [0, 0, 0], [0, 0, 0], [0, 0, 0]
     ];
 
     const makeMove = function (row, column, player) {
-        board[row][column] = player
+
+        if (board[row][column] === 0) {
+            board[row][column] = player.piece
+        }
     };
 
     const printBoard = () => {
         console.log(board)
     }
+
+
     return { board, makeMove, printBoard };
 })()
-
-
 
 function gameController(playerOne, playerTwo) {
     playerOneName = playerOne
     playerTwoName = playerTwo
+    const board = GameBoard
 
     const players = [
         {
@@ -40,26 +44,56 @@ function gameController(playerOne, playerTwo) {
         }
     }
     const getActivePlayer = () => {
-        return activePlayer
+        activePlayer
+        console.log(`It is ${activePlayer.name}'s turn.`)
     }
-
-    const board = Gameboard
 
     const printRound = () => {
         board.printBoard()
-        console.log(`It is ${activePlayer}'s turn.`)
     }
 
-    const playRound = (row, column) => {
+    const playRound = () => {
+        let tie = False
+        let winner = null
 
         getActivePlayer()
         printRound()
-        board.makeMove(1, 1, activePlayer.piece)
-        console.log(`${activePlayer.name} is making a move on row:${row} and column${column}`)
-
-
-
+        board.makeMove(0, 0, activePlayer)
+        checkForWinner(board.board, activePlayer.piece)
+        checkForTie(board.board)
         switchPlayerTurn()
+        printRound()
+    }
+
+    const checkForWinner = function (board, piece) {
+        if (
+            // Horizontal winners
+            (board[0][0] === piece && board[0][1] === piece && board[0][2] === piece) ||
+            (board[1][0] === piece && board[1][1] === piece && board[1][2] === piece) ||
+            (board[2][0] === piece && board[2][1] === piece && board[2][2] === piece) ||
+            // Vertical winners
+            (board[0][0] === piece && board[1][0] === piece && board[2][0] === piece) ||
+            (board[0][1] === piece && board[1][1] === piece && board[2][1] === piece) ||
+            (board[0][2] === piece && board[1][2] === piece && board[2][2] === piece) ||
+            // Diagonal winners
+            (board[0][0] === piece && board[1][1] === piece && board[2][2] === piece) ||
+            (board[0][2] === piece && board[1][1] === piece && board[2][0] === piece)
+        ) {
+            console.log("We have a winner");
+        } else {
+            console.log("No winner yet");
+        }
+    }
+
+    const checkForTie = function (board) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (board[i][j] === 0) {
+                    return False
+                }
+            }
+        }
+        return True
     }
     return { playRound }
 
